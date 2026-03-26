9c0a7bb502ecedac411f3a341a1492b6d0a113ad
wellingtonhzt <106972710+wellingtonhzt@users.noreply.github.com>
2026-03-26 08:58:29 -0300

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "WL Infra";
+export const SITE_DESCRIPTION = "Infraestrutura de TI & Cloud - Wellington Lima";
